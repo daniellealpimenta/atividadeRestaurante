f75e4e88a2f64dc6bc18fd5bc3e42f53f101be05
Daniel Leal Pimenta <129066504+daniellealpimenta@users.noreply.github.com>
2024-09-12 11:11:36 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,22 +1,24 @@
 import java.util.ArrayList;
 
-
 public class Pedido {
     private ArrayList<ItemMenu> itens;
     private Cliente cliente;
     private double total;
     private String status;
 
-    public Pedido(Cliente cliente) {
-        this.itens = new ArrayList<>();
-        this.cliente = cliente;
-        this.total = 0.0;
-        this.status = "Em preparação"; // Status inicial padrão
+    public Pedido() {
+        this.itens = new ArrayList<>(); // Inicializa a lista de itens
+        this.total = 0.0; // Inicializa o total com 0
+        this.status = ""; // Inicializa o status com uma string vazia
     }
 
     public void adicionarItem(ItemMenu item){
         itens.add(item);
-        total += item.calcularPreco();
+        total++;
+    }
+
+    public void mostrarCarrinho(int i){
+        System.out.println("" + itens.get(i));
     }
 
     public double calcularTotal(){
@@ -24,7 +26,10 @@ public class Pedido {
     }
 
     public void atualizarStatus(String novoStatus){
-        this.status = novoStatus;
+        System.out.println(this.status = novoStatus);
     }
 
+    public double getTotal() {
+        return total;
+    }
 }
