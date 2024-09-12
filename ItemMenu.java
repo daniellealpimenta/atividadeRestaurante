bc858eee9d8281bc796a447124c0ad0c4b78f57c
Ana Julia <129068318+wnajulha@users.noreply.github.com>
2024-09-12 10:26:17 -0300

    Add files via upload

---
+++ ItemMenu.java
@@ -0,0 +1,25 @@
+
+public class ItemMenu {
+    private String  nome;
+    private String categoria;
+    private double preco ;
+
+    public ItemMenu(String nome, String categoria, double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public double getPreco() {
+        return this.preco;
+    }
+    }
+
